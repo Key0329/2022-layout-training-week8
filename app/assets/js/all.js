768feb1831c57a72a96e91f6e17517b1e3ce43e9
Key <springfield0329@gmail.com>
2022-09-02 18:30:47 +0800

    header nav rwd 100%

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,21 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+
+  // navbar ham menu
+  $(".navbar-toggler-logo").click(function (e) {
+    event.preventDefault();
+    $(".fa-bars-logo").toggleClass("d-none");
+    $(".fa-xmark-logo").toggleClass("d-none");
+  })
+
+  // navbar magnifying search
+  $(".navbar-glass-mobile").click(function (e) {
+    event.preventDefault();
+    $(".navbar-toggler-logo, .navbar-brand-logo, .navbar-previous, .navbar-searchBox").toggleClass("d-none");
+  })
+  // navbar previous
+  $(".navbar-previous").click(function (e) {
+    event.preventDefault();
+    $(".navbar-toggler-logo, .navbar-brand-logo, .navbar-previous, .navbar-searchBox").toggleClass("d-none");
+  })
+
 });

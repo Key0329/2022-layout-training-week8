734e3f9d53098315ad6b54db2f5daa0ab21ba69e
Key <springfield0329@gmail.com>
2022-09-02 18:31:47 +0800

    Update 2022-09-02T10:31:41.649Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,21 +1,21 @@
-$(document).ready(function () {
+"use strict";
 
+$(document).ready(function () {
   // navbar ham menu
   $(".navbar-toggler-logo").click(function (e) {
     event.preventDefault();
     $(".fa-bars-logo").toggleClass("d-none");
     $(".fa-xmark-logo").toggleClass("d-none");
-  })
+  }); // navbar magnifying search
 
-  // navbar magnifying search
   $(".navbar-glass-mobile").click(function (e) {
     event.preventDefault();
     $(".navbar-toggler-logo, .navbar-brand-logo, .navbar-previous, .navbar-searchBox").toggleClass("d-none");
-  })
-  // navbar previous
+  }); // navbar previous
+
   $(".navbar-previous").click(function (e) {
     event.preventDefault();
     $(".navbar-toggler-logo, .navbar-brand-logo, .navbar-previous, .navbar-searchBox").toggleClass("d-none");
-  })
-
+  });
 });
+//# sourceMappingURL=all.js.map

a735cde74610aa250dca47900d240cd947f727b5
Key <springfield0329@gmail.com>
2022-09-04 01:01:37 +0800

    index artist rwd 100%

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -18,4 +18,26 @@ $(document).ready(function () {
     $(".navbar-toggler-logo, .navbar-brand-logo, .navbar-previous, .navbar-searchBox").toggleClass("d-none");
   })
 
+
+  // index swiper
+
+  let swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 24,
+    // loop: true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    }
+  });
+
+
+
 });
